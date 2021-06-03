2ce32da52f8937d3fde21bd9b6934ec76f880805
Kseniys <kipashina@edu.hse.ru>
2021-06-03 23:36:07 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/testlib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testlib.dir/main.cpp.obj"
+  "libtestlib.dll.a"
+  "testlib.exe"
+  "testlib.exe.manifest"
+  "testlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
